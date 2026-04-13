1f5900b0923b365ed309f6c60f41e93e6f7e9846
SantiagoRR2004 <114857078+SantiagoRR2004@users.noreply.github.com>
2026-04-13 19:25:59 +0200

    Download only once

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import requests
+import random
 import json
+import time
+import tqdm
 import os
 
 
-def fetch_reviews(id: int, videogame: str) -> None:
+def fetchReviews(id: int, videogame: str) -> None:
     """
     Fetches reviews for a given video game from the Steam store
     and saves them as JSONL files.
+
+    https://partner.steamgames.com/doc/store/getreviews
 
     Args:
         - id (int): The Steam app ID of the video game.
@@ -23,31 +28,52 @@ def fetch_reviews(id: int, videogame: str) -> None:
     parameters = {"filter": "all", "language": "english", "num_per_page": 100}
 
     for reviewType in ["positive", "negative"]:
-        parameters["review_type"] = reviewType
 
-        response = requests.get(url, params=parameters)
+        outputPath = os.path.join(
+            dataDirectory, f"{videogame}{reviewType.capitalize()}.jsonl"
+        )
 
-        if response.status_code == 200:
-            data = response.json()
+        # Only fetch reviews if the output file doesn't already exist
+        if not os.path.exists(outputPath):
 
-            outputPath = os.path.join(
-                dataDirectory, f"{videogame}{reviewType.capitalize()}.jsonl"
-            )
-            with open(outputPath, "w", encoding="utf-8") as file:
+            parameters["review_type"] = reviewType
+            response = requests.get(url, params=parameters)
+            time.sleep(random.random())
 
-                for review in data["reviews"]:
-                    important = {
-                        "score": float(review["weighted_vote_score"]),
-                        "review": review["review"],
-                    }
-                    json.dump(important, file, ensure_ascii=False)
-                    file.write("\n")
+            if response.status_code == 200:
+                data = response.json()
+
+                if data["success"] == 1:
+
+                    with open(outputPath, "w", encoding="utf-8") as file:
+
+                        for review in data["reviews"]:
+                            important = {
+                                "score": float(review["weighted_vote_score"]),
+                                "review": review["review"],
+                            }
+                            json.dump(important, file, ensure_ascii=False)
+                            file.write("\n")
 
 
-currentDirectory = os.path.dirname(os.path.abspath(__file__))
-videogamesPath = os.path.join(currentDirectory, "videogames.json")
-with open(videogamesPath, "r", encoding="utf-8") as file:
-    videogames = json.load(file)
+def getAllGames() -> None:
+    """
+    Fetches reviews for all video games listed in the "videogames.json" file.
 
-for videogame, id in videogames.items():
-    fetch_reviews(id, videogame)
+    Args:
+        - None
+
+    Returns:
+        - None
+    """
+    currentDirectory = os.path.dirname(os.path.abspath(__file__))
+    videogamesPath = os.path.join(currentDirectory, "videogames.json")
+
+    with open(videogamesPath, "r", encoding="utf-8") as file:
+        videogames = json.load(file)
+
+    for videogame, id in tqdm.tqdm(videogames.items(), desc="Fetching reviews"):
+        fetchReviews(id, videogame)
+
+
+getAllGames()
