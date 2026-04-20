dd203e0a596a4f66bd98a2be00324bf9e27316f5
SantiagoRR2004 <114857078+SantiagoRR2004@users.noreply.github.com>
2026-04-20 19:00:13 +0200

    No empty files

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import dataAcquisition
+import dataHandling
+import topicModeling
 
 if __name__ == "__main__":
     dataAcquisition.updateVideogames()
     dataAcquisition.getAllGames()
+    dataHandling.onlyEnglish()
+    topicModeling.basicTopicModeling()
